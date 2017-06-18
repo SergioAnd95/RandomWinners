6eedab367571e61940616eb275b80c3b45c11aee
SergioAnd95 <developinc@yadnex.ru>
2017-06-18 18:43:50 +0300

    fix tests

--- candidates/tests.py
+++ candidates/tests.py
@@ -18,15 +18,13 @@ class RandomGenerateTestCase(TestCase):
         self.group.candidates.create(name='Вячеслав')
         self.group.candidates.create(name='Игорь')
         self.group.candidates.create(name='Владислав')
+        self.group.candidates.create(name='Прокофий')
 
     def test_generate_random_winners(self):
         self.assertEqual(self.group.get_winners().count(), 3)
 
     def test_refresh_random(self):
-        old_winners = self.group.get_winners()
-        new_winners = self.group.refresh_winners()
-
-        self.assertNotEqual(old_winners, new_winners)
+        self.assertNotEqual(list(self.group.get_winners()), list(self.group.refresh_winners()))
 
     def test_add_same_candidate_to_group(self):
         with self.assertRaises(IntegrityError):
